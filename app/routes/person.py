08e5e8e3b7480de1e8131756cb32d98a2e68eaf9
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-10-18 21:32:41 +0200

    reorganized order of person endpoints | minor fixes

--- app/routes/person.py
+++ app/routes/person.py
@@ -1,7 +1,6 @@
-from pprint import pprint
 from typing import List
 
-from fastapi import APIRouter, Body, HTTPException, Query
+from fastapi import APIRouter, Body, HTTPException
 from neo4j import Result
 from neo4j.exceptions import ServiceUnavailable
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED
@@ -12,13 +11,37 @@ from app.db.driver import Driver
 router = APIRouter()
 
 
+@router.get(
+    "/person/person_name={person_name}",
+    status_code=HTTP_200_OK,
+    tags=["person"],
+)
+async def get_person(person_name: str) -> Person:
+    with Driver.session() as session:
+        result: Person = session.read_transaction(_find_and_return_person, person_name)
+        if not result:
+            raise HTTPException(status_code=404, detail="Not found")
+
+    return result
+
+
+@router.get(
+    "/person",
+    status_code=HTTP_200_OK,
+    tags=["person"],
+)
+async def get_all_person() -> List[Person]:
+    with Driver.session() as session:
+        result: List[Person] = session.read_transaction(_return_all_person)
+    return result
+
+
 @router.post(
     "/person",
     status_code=HTTP_201_CREATED,
     tags=["person"],
-    response_model=Person
 )
-async def create(person: Person = Body(..., embed=True)):
+async def create_person(person: Person = Body(..., embed=True)) -> Person:
     with Driver.session() as session:
         if session.read_transaction(_find_and_return_person, person.name):
             raise HTTPException(status_code=404, detail="Person exist")
@@ -26,44 +49,20 @@ async def create(person: Person = Body(..., embed=True)):
     return result
 
 
-@router.get(
-    "/person/{person_name}",
-    status_code=HTTP_200_OK,
-    tags=["person"],
-    response_model=Person
-)
-async def get_person(person_name: str = Query(...)):
-    with Driver.session() as session:
-        result: Person = session.read_transaction(_find_and_return_person, person_name)
-
-    return result
-
-
-@router.get(
+@router.delete(
     "/person",
     status_code=HTTP_200_OK,
     tags=["person"],
-    response_model=List[Person]
 )
-async def get_all_person():
+async def delete_person(person_name: str):
     with Driver.session() as session:
-        result: List[Person] = session.read_transaction(_return_all_person)
-    return result
+        session.write_transaction(_delete_person, person_name)
 
 
-@router.delete(
-    "/person/{person_name}",
-    status_code=HTTP_200_OK,
-    tags=["person"],
-)
-async def delete_person(person_name: str = Query(...)):
-    with Driver.session() as session:
-        session.read_transaction(_delete_and_return_person, person_name)
-
-
-def _delete_and_return_person(tx, person_name: str):
+def _delete_person(tx, person_name: str):
     query = (
-        "MATCH (p:Person {name: $person_name}) DELETE p"
+        "MATCH (p:Person {name: $person_name}) "
+        "DELETE p"
     )
     tx.run(query, person_name=person_name)
 
@@ -76,14 +75,17 @@ def _find_and_return_person(tx, person_name: str) -> Person:
     )
     result: Result = tx.run(query, person_name=person_name)
     try:
-        return Person(**result.single()['p'])
+        person_data = result.single()
+        if person_data:
+            return Person(**person_data['p'])
     except ServiceUnavailable as exception:
         raise exception
 
 
 def _return_all_person(tx) -> List[Person]:
     query = (
-        "MATCH (p:Person) RETURN p"
+        "MATCH (p:Person) "
+        "RETURN p"
     )
     result: Result = tx.run(query)
     try:
@@ -94,7 +96,8 @@ def _return_all_person(tx) -> List[Person]:
 
 def _create_and_return_person(tx, person: Person) -> Person:
     query = (
-        "CREATE (p:Person {name:$name , city:$city, phone:$phone}) RETURN p"
+        "CREATE (p:Person {name:$name , city:$city, phone:$phone}) ",
+        "RETURN p"
     )
     result: Result = tx.run(query, **person.dict())
     try:
