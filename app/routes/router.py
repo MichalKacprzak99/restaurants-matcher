ba4e3019e27880fa6f4e5ff52135577db0fa49d9
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-10-31 18:29:28 +0100

    restaurant endpoints implemented

--- app/routes/router.py
+++ app/routes/router.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from app.routes.person import router as person_router
 from app.routes.friendship import router as friendship_router
 from app.routes.cuisine import router as cuisine_router
+from app.routes.restaurant import router as restaurant_router
 
 router = APIRouter()
 
 router.include_router(router=cuisine_router)
 router.include_router(router=friendship_router)
 router.include_router(router=person_router)
+router.include_router(router=restaurant_router)
