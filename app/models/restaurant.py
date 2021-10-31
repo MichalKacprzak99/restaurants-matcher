0eef674692f808b6b77f1629fef9ff82588ad5fd
MichalKacprzak99 <michal.kacprzak999@gmail.com>
2021-10-31 18:29:28 +0100

    restaurant endpoints implemented

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -1,10 +1,13 @@
+from typing import Optional
+
 from fastapi_utils.api_model import APIModel
 
 from app.models.cuisine import Cuisine
 from app.models.person import Person
 
 
+# TODO temporary optional
 class Restaurant(APIModel):
-    cuisine: Cuisine
+    cuisine: Optional[Cuisine]
     name: str
-    owner: Person
+    owner: Optional[Person]
